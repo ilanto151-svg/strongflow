545c3e9c24d5ea4b5d7c248612e8e6a0f2020a0c
Ilan Taussig <ilanato151@gmail.com>
2026-03-21 09:35:47 +0200

    Debug env

--- server/pg.js
+++ server/pg.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Pool } = require('pg');
-
+console.log('DATABASE_URL:', process.env.DATABASE_URL);
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production'
